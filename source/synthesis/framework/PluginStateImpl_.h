db3cb1f608519626166cc597298805ccb26dac1f
Davis Polito <davispolito1@gmail.com>
2025-02-13 15:13:02 -0500

    updating leaf modules to compile in c

--- source/synthesis/framework/PluginStateImpl_.h
+++ source/synthesis/framework/PluginStateImpl_.h
@@ -49,8 +49,8 @@ struct LEAFParams : public chowdsp::ParamHolder
         std::array<float,MAX_NUM_PARAMS> mutable_params = empty_params;
 
         void** yeet = reinterpret_cast<void**>(&module);
-        module_init_map[map.get<T>()](reinterpret_cast<void**>(&module), mutable_params.data(), getNextUuid(leaf) , leaf);
-        proc_init_map[map.get<T>()](reinterpret_cast<void*>(module), &processor);
+        leaf::module_init_map[map.get<T>()](reinterpret_cast<void**>(&module), mutable_params.data(), getNextUuid(leaf) , leaf);
+        leaf::proc_init_map[map.get<T>()](reinterpret_cast<void*>(module), &processor);
     }
     leaf::tProcessor processor;
     T* module;
