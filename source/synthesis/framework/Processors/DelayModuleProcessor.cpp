73b518772ab00311d02fb38029a4480e2285c76b
Myra <mmnorton@princeton.edu>
2026-01-07 11:27:33 -0500

    delay?

---
+++ source/synthesis/framework/Processors/DelayModuleProcessor.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Myra Norton on 7/25/25.
+//
+
+#include "DelayModuleProcessor.h"
---
+++ source/synthesis/framework/Processors/DelayModuleProcessor.h
@@ -0,0 +1,16 @@
+//
+// Created by Myra Norton on 7/25/25.
+//
+
+#ifndef DELAYMODULEPROCESSOR_H
+#define DELAYMODULEPROCESSOR_H
+
+
+
+class DelayModuleProcessor {
+
+};
+
+
+
+#endif //DELAYMODULEPROCESSOR_H
