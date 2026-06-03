d905fb60becc2efbe8787495ff450330f8f0538f
Mr. Kahoobadoo <60197309+MrKahoobadoo@users.noreply.github.com>
2026-06-03 12:47:59 -0400

    Fixed Windows Compatibility issue
    Added windows conditional around usage of "setMaxMainMenu"

--- source/standalone/synth_editor.cpp
+++ source/standalone/synth_editor.cpp
@@ -87,18 +87,22 @@ SynthEditor::SynthEditor(bool use_gui) : SynthGuiInterface(this, use_gui) {
   }
   // mainmenumodel on mac
 
+    #if !_WIN32 && !_WIN64
   menuModel = std::make_unique<MainMenuModel>(commandManager);
     jassert(menuModel != nullptr);
   juce::MenuBarModel::setMacMainMenu(menuModel.get());
+    #endif
 }
 
 SynthEditor::~SynthEditor() {
 #if PERFETTO
   MelatoninPerfetto::get().endSession();
 #endif
-  juce::PopupMenu::dismissAllActiveMenus();
+  #if !_WIN32 && !_WIN64
+    juce::PopupMenu::dismissAllActiveMenus();
   shutdownAudio();
   juce::MenuBarModel::setMacMainMenu(nullptr);
+    #endif
 }
 void SynthEditor::prepareToPlay(int buffer_size, double sample_rate) {
   //engine_->setSampleRate(sample_rate);
