b5ce016093eb1251af8f5ec7500d8da1fda8c01e
Mr. Kahoobadoo <60197309+MrKahoobadoo@users.noreply.github.com>
2026-07-10 19:38:59 -0400

    Finished draft of NoiseModule.h/cpp and NoiseModuleProcessor.h

--- source/synthesis/framework/Processors/NoiseModuleProcessor.h
+++ source/synthesis/framework/Processors/NoiseModuleProcessor.h
@@ -4,5 +4,107 @@
 
 #ifndef ELECTORSYNTH_NOISEMODULEPROCESSOR_H
 #define ELECTORSYNTH_NOISEMODULEPROCESSOR_H
+#include "NoiseModule.h"
+#include "../PluginStateImpl_.h"
+#include "ParameterView/ParametersView.h"
+#include "Identifiers.h"
+#include "ProcessorBase.h"
+
+struct NoiseParams : public LEAFParams<_tNoiseModule>
+{
+    NoiseParams(LEAF* leaf) : LEAFParams<_tNoiseModule>(leaf)
+    {
+        add(gain, tilt, peakGain, peakFreq);
+    }
+
+    //add env watch param so that it isnt null
+    chowdsp::FloatParameter::Ptr envwatchparam {
+        juce::ParameterID { "watch", 100 },
+        "watch",
+        chowdsp::ParamUtils::createNormalisableRange (0.0f, 1.0f, 0.5f),
+        1.0f,
+        all_params[0],
+        [this] (float val) {
+            // for (auto mod: modules) mod->setterFunctions[EnvParams::EnvSustain](mod, val);
+        },
+        &chowdsp::ParamUtils::floatValToString,
+        &chowdsp::ParamUtils::stringToFloatVal
+    };
+
+    // gain
+    chowdsp::GainDBParameter::Ptr gain {
+        juce::ParameterID{"gain", 100},
+        "Gain",
+        chowdsp::ParamUtils::createNormalisableRange(0.0f, 2.0f, 1.0f),
+        1.f,
+        all_params[NosParams::NoiseGain],
+        [this](float val)
+        {for (auto mod: modules)    tNoiseModule_setParameter(mod,NoiseGain,val);
+        },
+    };
+
+    // tilt
+    chowdsp::FloatParameter::Ptr tilt {
+        juce::ParameterID { "tilt", 100 },
+        "Tilt",
+        chowdsp::ParamUtils::createNormalisableRange (0.0f, 1.0f, 0.5f),
+        1.0f,
+        all_params[NosParams::NoiseTilt],
+        [this] (float val)
+        {for (auto mod: modules) tNoiseModule_setParameter(mod, NoiseTilt,val);
+        },
+        &chowdsp::ParamUtils::floatValToString,
+        &chowdsp::ParamUtils::stringToFloatVal
+    };
+
+    // peakGain
+    chowdsp::GainDBParameter::Ptr peakGain {
+        juce::ParameterID{"peakGain", 100},
+        "PeakGain",
+        chowdsp::ParamUtils::createNormalisableRange(0.0f, 2.0f, 1.0f),
+        1.f,
+        all_params[NosParams::NoisePeakGain],
+        [this](float val)
+        {for (auto mod: modules)    tNoiseModule_setParameter(mod,NoisePeakGain,val);
+        },
+    };
+
+    // peakFreq
+    chowdsp::FloatParameter::Ptr peakFreq {
+        juce::ParameterID { "peakFreq", 100 },
+        "PeakFreq",
+        chowdsp::ParamUtils::createNormalisableRange (0.0f, 1.0f, 0.5f),
+        1.0f,
+        all_params[NosParams::NoisePeakFreq],
+        [this] (float val)
+        {for (auto mod: modules) tNoiseModule_setParameter(mod, NoisePeakFreq, val);
+        },
+        &chowdsp::ParamUtils::floatValToString,
+        &chowdsp::ParamUtils::stringToFloatVal
+    };
+
+};
+
+class NoiseModuleProcessor : public ProcessorStateBase<PluginStateImpl_<NoiseParams>>
+{
+public:
+    NoiseModuleProcessor(electrosynth::SoundEngine* engine,const juce::ValueTree&, LEAF* leaf,juce::UndoManager*);
+
+    void getNextAudioBlock (const juce::AudioSourceChannelInfo &bufferToFill) override {}
+    void processBlock (juce::AudioBuffer<float>&, juce::MidiBuffer&) override;
+    void prepareToPlay (int samplesPerBlock, double sampleRate ) override {};
+    void releaseResources() override {}
+    //void processAudioBlock (juce::AudioBuffer<float>& buffer) override {};
+    //    bool acceptsMidi() const override
+    //    {
+    //       return true;
+    std::unique_ptr<SynthSection> createEditor() override
+    {
+        return std::make_unique<electrosynth::ParametersView>(state_, state_.params, state.getProperty(IDs::type).toString() + state.getProperty(IDs::uuid).toString());
+    }
+    // juce::AudioProcessorEditor* createEditor() override {return new electrosynth::ParametersViewEditor{*this,vstate.getProperty(IDs::type).toString() + vstate.getProperty(IDs::uuid).toString()};};
+    chowdsp::ScopedCallbackList callbacks;
+
+};
 
 #endif // ELECTORSYNTH_NOISEMODULEPROCESSOR_H
