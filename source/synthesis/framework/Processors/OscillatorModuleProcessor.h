21af15408d07c16302ae802af57fc160d4762e04
Mr. Kahoobadoo <60197309+MrKahoobadoo@users.noreply.github.com>
2026-07-23 00:25:42 -0400

    Fixed sound_engine issue where every voice got its own tSimplePoly. Now there's just one for all 12 voices. Next, note glide in simpleOscillatorModule is no longer by the voice, it is now calculated with all 12 voices in mind, using the most recent voice's midi note as the origin point for all following notes to glide from. Still unfinished; many bugs need to be fixed still

--- source/synthesis/framework/Processors/OscillatorModuleProcessor.h
+++ source/synthesis/framework/Processors/OscillatorModuleProcessor.h
@@ -9,6 +9,7 @@
 #include "ParameterView/ParametersView.h"
 #include "Identifiers.h"
 #include "ProcessorBase.h"
+#include "leaf-midi.h"
 namespace electrosynth{
     namespace utils
     {
@@ -280,6 +281,8 @@ public:
     }
    // juce::AudioProcessorEditor* createEditor() override {return new electrosynth::ParametersViewEditor{*this,vstate.getProperty(IDs::type).toString() + vstate.getProperty(IDs::uuid).toString()};};
     chowdsp::ScopedCallbackList callbacks;
+
+    tStack activeModules;
 
 };
 
